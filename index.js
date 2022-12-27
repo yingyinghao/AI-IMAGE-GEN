57cbcfc42972c4e6d566a282a3855b8429089e2f
yingying <yingying.hao313@gmail.com>
2022-12-26 22:37:51 -0500

    update

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Enable body parser
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-//set static file
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/openai', require('./routes/openaiRoutes'))
+app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- js/main.js
+++ js/main.js
@@ -1,60 +1,59 @@
-const { generateimage } = require("../openaiControllers/openaiController");
-
-function onSubmit(e){
+function onSubmit(e) {
   e.preventDefault();
 
-  document.querySelector('.nsg').textContent = '';
+  document.querySelector('.msg').textContent = '';
   document.querySelector('#image').src = '';
 
   const prompt = document.querySelector('#prompt').value;
   const size = document.querySelector('#size').value;
 
-  if(prompt === ''){
+  if (prompt === '') {
     alert('Please add some text');
     return;
   }
 
   generateImageRequest(prompt, size);
 }
-async function generateImageRequest(prompt, size){
-try{
-  showSpinner();
 
-  const response = await fetch('/openai/generateimage',{
-    method: 'POST',
-    headers:{
-      'Content-Type': 'application/json'
-    },
-    body:JSON.stringify({
-      prompt,
-      size
-    })
-  });
-  if(!response.ok){
+async function generateImageRequest(prompt, size) {
+  try {
+    showSpinner();
+
+    const response = await fetch('/openai/generateimage', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt,
+        size,
+      }),
+    });
+
+    if (!response.ok) {
+      removeSpinner();
+      throw new Error('That image could not be generated');
+    }
+
+    const data = await response.json();
+    // console.log(data);
+
+    const imageUrl = data.data;
+
+    document.querySelector('#image').src = imageUrl;
+
     removeSpinner();
-    throw new Error('That image coule not be generated');
+  } catch (error) {
+    document.querySelector('.msg').textContent = error;
   }
-  const data = await response.json();
-  console.log(data);
-  const imageUrl = data.data;
-
-  document.querySelector('#image').src = imageUrl;
-
-
-  removeSpinner();
-
-}catch(error){
-  document.querySelector('.msg').textContent = error;
-}
 }
 
-function showSpinner(){
+function showSpinner() {
   document.querySelector('.spinner').classList.add('show');
-
 }
 
-function removeSpinner(){
+function removeSpinner() {
   document.querySelector('.spinner').classList.remove('show');
 }
 
-document.querySelector('#image-form').addEventListener('submit', onsubmit);
+document.querySelector('#image-form').addEventListener('submit', onSubmit);
